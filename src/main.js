2f780d042947d07223f3340bc13ed8772f6cf69f
samdevega <s.a.devega.garcia@gmail.com>
2021-12-05 21:16:44 +0000

    🚧 implement base button component

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { createApp } from 'vue'
 import router from './router.js'
 import store from './store/'
 import App from './App.vue'
+import BaseButton from './components/ui/BaseButton.vue'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.component('base-button', BaseButton)
 app.mount('#app')
